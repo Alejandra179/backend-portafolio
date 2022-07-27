5df68788593cdfc6dad95cddc59221a37247f3f5
Alejandra179 <69905852+Alejandra179@users.noreply.github.com>
2022-07-27 11:22:31 -0300

    Delete src directory

--- src/main/java/com/personal/miportafolio/security/Enums/RolNombre.java
+++
@@ -1,9 +0,0 @@
-package com.personal.miportafolio.security.Enums;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties({"hibernateLazyInitializer"})
-public enum RolNombre {
-
-		ROLE_ADMIN,ROLE_USER
-}
